65165295afe6b0f1d5715987c98f2e9585b2c5a2
Vishnu Vardhan Polanki <124869130+VishnuVardhan8055@users.noreply.github.com>
2025-10-31 08:35:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 @app.get("/")
 def home():
-    return "Hello welcome to the Home Page""For Crud Operations use /docs path "
+    return "Hello welcome to the Home Page. For Crud Operations use /docs path "
 
 @app.get("/books/", response_model=list[schema.Book])
 def get_all_books(db: Session = Depends(get_db)):
